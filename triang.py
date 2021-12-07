7a8db8f03c063d786acf558edf05884b9fbba147
AlexDosy <alexdosyv210@gmail.com>
2021-12-07 22:05:34 +0530

    .

---
+++ triang.py
@@ -0,0 +1,33 @@
+import numpy as np
+from math import pi
+import matplotlib.pyplot as plt
+from numpy import newaxis
+import time
+def disp(x):
+    with np.printoptions(precision=2,suppress=Time):
+        print(x)
+
+
+def gen_dft_mtx(N,disp_flag=1):
+    row_index=np.array([0,1,2,3]) 
+    col_index=row_index.T
+
+
+    if(disp_flag):
+        print("\n row index=",row_index)
+        print("\n row index shape:",row_index.shape)
+
+        print("col index=\n",col_index)
+        print("col index shape:",col_index.shape)
+        
+
+    index_matrix=col_index@row_index 
+    dft_mtx=np.exp(-1j*2*pi*index_matrix/N)
+    if(disp_flag):
+        print("\n\n index matrix:\n",index_matrix)
+        print("\n DFT matrix:\n")
+
+        disp(dft_mtx)
+    return(dft_mtx)
+   N=4
+   dft_mtx=gen_dft_mtx(N,1)
